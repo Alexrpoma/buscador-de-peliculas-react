918071d91354efef968a05cb0bf6b2bf8915ab0d
Alex Poma <alex.rpoma@outlook.com>
2023-05-11 09:14:02 -0400

    Avoid repeat search

--- src/hooks/useMovies.ts
+++ src/hooks/useMovies.ts
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import type { Movie, Error } from '../posterMoviesApi'
 import { searchMovies } from '../services/movies'
 
@@ -15,9 +15,12 @@ interface Search {
 export const useMovies = ({ search }: Search): MoviesData => {
   const [foundMovies, setFoundMovies] = useState<Movie[]>()
   const [errorMovie, setErrorMovie] = useState<Error>()
+  const previousSearch = useRef<string | undefined>(search)
 
   const getMovies = async (): Promise<void> => {
+    if (previousSearch.current === search) return
     if (search.length > 0) {
+      previousSearch.current = search
       const result = await searchMovies({ search })
       setFoundMovies(result.movies)
       setErrorMovie(result.error)
